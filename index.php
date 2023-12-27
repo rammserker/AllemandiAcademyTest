f70b55d03d5bafefcc0d56681775ca642c7c5b57
rammserker <rammserker@gmail.com>
2023-12-27 06:01:41 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tito Allemandi Test</title>
+    </head>
+    <body>
+        <header>
+            <h1>Tito Allemandi Test</h1>
+        </header>
+        <main>
+            <nav>
+            </nav>
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
